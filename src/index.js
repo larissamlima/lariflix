396b9d9b31dc2b285fda75b2b4e7b921906e8342
Larissa Lima <larissa.lima@dcomp.sor.ufscar.br>
2020-08-01 12:49:38 -0300

    inicio aula2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
-);
+function CadastroVideo() {
+    return ( <
+        div >
+        Página de cadastro de Video <
+        /div>
+    )
+}
+
+ReactDOM.render(
+
+        <
+        BrowserRouter >
+        <
+        Switch >
+        <
+        Route path = "/cadastro/video"
+        component = { CadastroVideo }
+        /> <
+        Route path = "/"
+        component = { App }
+        exact /
+        >
+        <
+        Route component = {
+            () => ( < div > Página 404 < /div>)
+            }
+            />
+
+            <
+            /Switch>  < /
+            BrowserRouter > ,
+
+            document.getElementById('root')
+        );
