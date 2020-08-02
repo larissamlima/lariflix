1dee666a99d4ab4d74439cfb0b92b28117bec8aa
Larissa Lima <larissa.lima@dcomp.sor.ufscar.br>
2020-08-02 02:32:20 -0300

    finalização aula 2

--- src/index.js
+++ src/index.js
@@ -4,33 +4,31 @@ import './index.css';
 import Home from './pages/Home';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import  CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 
-const Pagina404 = () => ( < div > Página 404 < /div>)
+const Pagina404 = () => ( < div > Página 404 </div>)
 
 ReactDOM.render(
 
-        <
-        BrowserRouter >
-        <
-        Switch >
-        <
-        Route path = "/cadastro/video"
-        component = { CadastroVideo }
-        /> <
-        Route path = "/"
+        <BrowserRouter >
+            <Switch >
+                <Route path = "/"
         component = { Home }
-        exact /
-        >
-        <
-        Route component = {
+        exact />
+                <Route path = "/cadastro/video"
+        component = { CadastroVideo }
+        />
+        <Route path = "/cadastro/categoria"
+        component = { CadastroCategoria }
+        />
+                
+                <Route component = {
             Pagina404
-            }
-            />
+            }/>
 
-            <
-            /Switch>  < /
-            BrowserRouter > ,
+            </Switch>  
+        </BrowserRouter > ,
 
             document.getElementById('root')
         );
