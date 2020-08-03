1d176755c288a3206dddaa1bc0c0cd8adca1b505
Larissa Lima <larissa.lima@dcomp.sor.ufscar.br>
2020-08-03 16:29:26 -0300

    metade aula 2

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-  return (
-    <PageDefault>
-      <h1>Cadastro de Categoria</h1>
+  const[categorias, setCategorias] = useState(['Teste']);
+  const [nomeDaCategoria,setNomeDaCategoria] = useState('Valor inicial');
 
-      <form>
+    return (
+    <PageDefault> 
+      <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
 
+      <form onSubmit={function handleSubmit(infosDoEvento){
+        infosDoEvento.preventDefault();
+        console.log('Você tentou enviar o form né?')
+        setCategorias([
+          ...categorias,
+          nomeDaCategoria
+        ]);
+      }}>
+      {/*state*/}
         <label>
           Nome da Categoria:
           <input
             type="text"
+            value={nomeDaCategoria}
+            onChange={function funcaoHandler(infosDoEvento){
+              setNomeDaCategoria(infosDoEvento.target.value);
+            }}
           />
         </label>
 
@@ -21,6 +35,15 @@ function CadastroCategoria() {
         </button>
       </form>
 
+      <ul>
+        {categorias.map( (categoria) => {
+          return(
+            <li key = {categoria}>
+              {categoria}
+            </li>
+          )
+        })}
+      </ul>
 
       <Link to="/">
         Ir para home
